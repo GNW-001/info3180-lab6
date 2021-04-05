ea8c55c6ee8d50b48c3ebd3dcd0e18c28467caa9
GNW-001 <garrik.wong@gmail.com>
2021-04-04 23:06:23 -0500

    Did everything. VueJS, CSS and routing

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,11 +1,92 @@
 /* Add your Application JavaScript */
-const app = Vue.createApp({
-  data() {
-    return {
-      welcome: 'Hello World! Welcome to VueJS'
+
+const Home = {
+    name: 'Home',
+    template: `
+    <div class="text-center">
+        <img src="/static/images/logo.png" alt="VueJS Logo">
+        <h1>{{ welcome }}</h1>
+    </div>
+    `,
+    data() {
+        return {
+            welcome: 'hello World! Welcome to VueJS'
+        }
     }
-  }
+};
+
+const NewsList = {
+    name: 'NewsList',
+    template: `<div class="news">
+            <h2>News</h2>
+
+            <div class="form-inline d-flex justify-content-center">
+                <div class="form-group mx-sm-3 mb-2">
+                    <label class="sr-only" for="search">Search</label>
+                    <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+                    <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+                    <br/><p>You are searching for {{ searchTerm }}</p>
+                </div>
+            </div>
+            <ul class="news__list">
+                <li v-for="article in articles" class="news__item"> <h4>{{ article.title }}</h4>  <div><img :src=article.urlToImage /><div><br/> {{article.description}}</li>
+            </ul>
+        </div>`,
+    created() {
+        let self = this;
+
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+            {
+                headers: {
+                    'Authorization': 'Bearer '
+                }
+            })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                self.articles = data.articles;
+            })
+    },
+    data() {
+        return {
+            articles: [],
+            searchTerm: ''
+        }
+    },
+    methods: {
+        searchNews() {
+            let self = this;
+            fetch('https://newsapi.org/v2/everything?q=' + self.searchTerm + '&language=en', {
+                headers: {
+                    'Authorization': 'Bearer 0c7a0e013c144f1fb390cca9ec6f4e14'
+                }
+            })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    self.articles = data.articles;
+                })
+        }
+    }
+};
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            welcome: 'Hello World! Welcome to VueJS'
+        }
+    },
+    components: {
+        'home': Home,
+        'news-list': NewsList
+    }
 });
+
 
 app.component('app-header', {
   name: 'AppHeader',
@@ -20,10 +101,10 @@ app.component('app-header', {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <router-link to="/" class="nav-link">Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                  <router-link to="/news" class="nav-link">News</router-link>
                 </li>
               </ul>
             </div>
@@ -33,22 +114,32 @@ app.component('app-header', {
   data: function() {
     return {};
   }
-});
+})
 
 app.component('app-footer', {
-  name: 'AppFooter',
-  template: `
+    name: 'AppFooter',
+    template: `
       <footer>
           <div class="container">
               <p>Copyright &copy {{ year }} Flask Inc.</p>
           </div>
       </footer>
   `,
-  data: function() {
-      return {
-          year: (new Date).getFullYear()
-      }
-  }
+    data: function () {
+        return {
+            year: (new Date).getFullYear()
+        }
+    }
 })
 
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes: [
+        { path: '/', component: Home },
+        { path: '/news', component: NewsList },
+    ]
+});
+
+app.use(router);
 app.mount('#app');
